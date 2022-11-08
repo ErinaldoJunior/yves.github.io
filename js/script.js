a803d3e63980757cca2bbe2ed860496c45824ce4
Erinaldo Junior <erinaldogomesjr@gmail.com>
2022-11-08 20:15:24 +0000

    js e favicon

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// Ativar links do menu interno
+
+const links = document.querySelectorAll(".produtos-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// Perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// Animação
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
